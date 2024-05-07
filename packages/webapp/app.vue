0f3227bec90bb68f8da59cd20fdd42b268d7e278
Miguel Franken <miguelfranken90@gmail.com>
2024-05-07 18:19:27 +0200

    feat: fetch siteree in webapp from cms

--- packages/webapp/app.vue
+++ packages/webapp/app.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+const { data: sitetree } = await useFetch('http://localhost:4000/sitetree')
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>Site Tree</h1>
+    <pre>{{ sitetree }}</pre>
   </div>
 </template>
