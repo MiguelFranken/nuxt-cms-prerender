bd3886f5a5496a54f3c1d3507f6f83790dad9665
Miguel Franken <miguelfranken90@gmail.com>
2024-05-07 19:23:40 +0200

    feat: programmatically add prerender routes via nuxt build-time hooks

--- packages/webapp/nuxt.config.ts
+++ packages/webapp/nuxt.config.ts
@@ -3,5 +3,10 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   routeRules: {
     '/hotels/cologne': { prerender: true },
+  },
+  hooks: {
+    'prerender:routes': async ({ routes }) => {
+      routes.add('/hotels/berlin')
+    }
   }
 })
