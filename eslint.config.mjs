38a938bca1648c5930398da094acd6ff09be7817
Miguel Franken <miguelfranken90@gmail.com>
2024-05-07 18:06:55 +0200

    chore: ignore ignored directories during linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,4 +2,11 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   vue: true,
+  gitignore: {
+    files: [
+      '.gitignore',
+      'packages/cms/.gitignore',
+      'packages/webapp/.gitignore',
+    ],
+  },
 })
